400ec86e1af6d5fd85032c832923b8b08d26ec08
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-07-25 16:52:18 -0400

    Array class constructor and first part of bucket expansion function.
    Class constructor that produces the initial array filled with as many objects waiting to be buckets as tell it to.
    
    Also a bucket expansion function that doubles the current array length and generates a new one of that length.
    
    Still need to have it copy the contents of the original array and buckets but that comes after actually put stuff in them.

--- main.js
+++ main.js
@@ -2,30 +2,94 @@
 //     throw new Error("Trying to access index out of bound");
 //   }
 
+
+function listTraverser(list, value) {
+  if (typeof(list) !== 'object' || list.head !== null ) {
+      alert("Please provide a LinkedList object")
+
+      return
+  }
+
+  let targetnode = list
+  let i = 0
+  while (targetnode.next !== null || i < 10000) {
+      if (targetnode.head === null && targetnode.next === null) {
+          return result = {'target': targetnode, 'depth': i}
+      }
+
+      if (targetnode.next === null && targetnode.data !== value && targetnode.head !== null) {
+          return result = {'target': targetnode, 'depth': i}
+      } 
+      
+      if(targetnode.data === value && targetnode.head !== null) {
+          return result = {'target': targetnode, 'depth': i, 'valuedepth': i}
+      }
+      i++;
+      targetnode = targetnode.next; 
+
+
+  }
+}
+
+
+
 // //    takes a key and produces a hash code with it. We did implement a fairly good hash function in the previous lesson. As a reminder:
 
 // Hash maps could accommodate various data types for keys like numbers, strings, objects. But for this project, only handle keys of type strings.
 
+class HashMap {
+  startingsize = 5
+  maxbuckets = 16
 
-class HashTable {
-    
-    hashMap = new Array (5)
+  constructor(size) {
+    this.nodes = new Array(size).fill({});
+    // return this.nodes; // Change this line to return this
+    return this.n; // Return this instead of this.nodes
+  }
+
+  // test(startingsize) {    for (let i = 0; i < size; i++) {
+  //   this.hashMap.push({})
+  // }}
+
 
     //hash function provided in the assignment edited to add modulo at return.
-    hash(key) {
+  hash(key, bucketcount) {
         let hashCode = 0;
            
         const primeNumber = 31;
-        const max = 17
         for (let i = 0; i < key.length; i++) {
-          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % max ;
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % bucketcount ;
         }
      
         return hashCode
       } 
-// takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten or we can say that we update the key’s value
-set(key, value) {
+      
+    //Example of how to calculate loadfactor
+    // loadFactor = this.numItems / this.table.length
+    // if loadfactor > .75 then resize = () => new Array(this.hashMap.length * 2)
+    //then loop through and copy over to the new larger array
+  loadfactor(bucketlist) {
+    if (this.maxbuckets / bucketlist.length >= .75) {
+      
+    } else {
+      
+    }
+  }
 
+
+  expandbuckets(bucketlist) {
+    let length  = bucketlist.nodes.length *= 2
+    let expanded = new HashMap(length)
+
+    //for loop through expanded transfering the contents of bucketlist over to it. Do I need to rehash it? Probably.
+    return expanded
+}
+// takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten or we can say that we update the key’s value
+
+//Attach it to the head of the linked list so dont have to traverse the list to add to it since order doesnt matter
+set(bucketlist) {
+  // console.table(bucketlist)
+  //check loadfactor to see if need to expand, if so do so first. Afterward or if not needed hash the input and add it to the top of the linked list in that bucket.
 
 
 }
@@ -99,6 +163,26 @@ entries(){
 
 
 // Testing values:
+
+let test1 = new HashMap(5) 
+console.log(test1.nodes.length)
+test1 = test1.expandbuckets(test1)
+console.log(test1.nodes.length)
+
+
+
+
+// const test = Object.create(HashMap.prototype);
+// HashMap.call(test, 5); // Call the constructor function with the desired size
+
+// console.table(test);
+// console.table(typeof(test))
+// console.table(test.length)
+
+// let test2 = []
+// console.log(Array.isArray(test))
+// let test2 = test.expandbuckets(test)
+// console.log(test2.length)
 
 // test.set('apple', 'red')
 // test.set('banana', 'yellow')
