d1bcc8f196f01e969859ea382398aeb17a6fc830
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-07-26 22:40:19 -0400

    Partially rewrote most of what already did.
    After more troubleshooting and research ended up having to tweak, change, or remove most of what already did.
    
    I think the constructor, set(), and expand() are now working correctly. Only thing unsure about is that when I added enough entries for the list to expand to 28 the highest index was only 13. So the hash() function might need tweaking.
    
    Still need to add duplicate detection/update. After that hopefully the rest will be standard LinkedList manipulation I am already familiar with just with multiple navigated to via hashing for index of which.

--- main.js
+++ main.js
@@ -8,7 +8,6 @@
 // Hash maps could accommodate various data types for keys like numbers, strings, objects. But for this project, only handle keys of type strings.
 
 class HashMap {
-  startingsize = 5
   maxbuckets = 7
 
   constructor(size) {
@@ -16,166 +15,159 @@ class HashMap {
       size = this.maxbuckets
     } 
 
-    this.nodes = new Array(size).fill(null).map(() => ({}));
-    return
+    this.buckets = new Array(size);
   }
 
 
-    //hash function provided in the assignment edited to add modulo at return.
+  //hash function provided in the assignment edited to add modulo at return.
   hash(key, bucketcount) {
         let hashCode = 0;
            
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
           hashCode = (primeNumber * hashCode + key.charCodeAt(i)) ;
-          // console.log(hashCode)
 
         }
-     console.log(hashCode  % bucketcount)
         return hashCode % bucketcount
       } 
       
-    //Example of how to calculate loadfactor
-    // loadFactor = this.numItems / this.table.length
-    // if loadfactor > .75 then resize = () => new Array(this.hashMap.length * 2)
-    //then loop through and copy over to the new larger array
-  loadfactor(bucketlist, bucket) {
-    if (bucketlist[bucket].length / bucketlist.maxbuckets  >= .75) {
-      return bucketlist.expandbuckets(bucketlist)
-    } 
-  }
-
 
   expandbuckets(bucketlist) {
-    bucketlist.maxbuckets  = bucketlist.nodes.length *= 2
+    bucketlist.maxbuckets = bucketlist.buckets.length *= 2
     let expanded = new HashMap(bucketlist.maxbuckets)
-    console.log(expanded)
+    expanded.maxbuckets = bucketlist.maxbuckets
 
-    for (const bucket of bucketlist.nodes) {
-      // console.log(bucket)
-      if (bucket.head === null) {
-        // console.log(bucket)
-        let currentnode = bucket
+    for (const bucket of bucketlist.buckets) {
+
+   
+      if (bucket !== undefined && bucket.head === null) {
+
+
+        let currentnode = bucket.next
         let i = 0
 
-        while (bucket.next !== null  || i < 10000) {   
-          // console.log(currentnode)
+        while (currentnode.next !== null  || i < 10000) {   
+ 
 
-          if (currentnode === null) {
-            // console.log(expanded)
-
-            break}
-
-          if (currentnode.head === null && currentnode.next !== null) {
-            currentnode = currentnode.next           
-          }
           bucketlist.set(expanded, currentnode.key, currentnode.value)                  
-          // console.log(expanded)
-          // console.log(currentnode)
+     
+
+           if (currentnode.next === null) {
+              break
+            }
           currentnode = currentnode.next
           i++
-        }
       }
+    }  
    
-
     }
-    console.log(expanded)
-    //for loop through expanded transfering the contents of bucketlist over to it. Do I need to rehash it? Probably.
-    //Doing it with array at first because it easier, change the bucekt arrays into linked lists later to make it easier to add and remove from.
+
     return expanded
 }
-// takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten or we can say that we update the key’s value
+// // takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten or we can say that we update the key’s value
 
-//Attach it to the head of the linked list so dont have to traverse the list to add to it since order doesnt matter
+// //Attach it to the head of the linked list so dont have to traverse the list to add to it since order doesnt matter
 set(bucketlist, key, value) {
-  //check loadfactor to see if need to expand, if so do so first. Afterward or if not needed hash the input and add it to the top of the linked list in that bucket.
-  let emptyObjectsCount = bucketlist.nodes.filter(node => Object.keys(node).length !== 0).length;
-  console.log(emptyObjectsCount / bucketlist.maxbuckets);
+
+
+  let emptyObjectsCount = bucketlist.buckets.filter(node => Object.keys(node).length !== 0).length;
 
   if (emptyObjectsCount / bucketlist.maxbuckets >= .75) {
       bucketlist.expandbuckets(bucketlist)  
 
   }
 
+  //I am guessing something off with this hashing function being the cause of it not spreading across the table as much as it should. When I manually ran a set after the table expanded the hash correctly worked. So maybe problem is in the function not being passed the right maxbucket value after expansion.
 
+  //log after hashed showing the correct higher values so again not sure why it seeming to use the hashed values from pre expansion.
+  // console.log(bucketlist.maxbuckets)
   let hashed = bucketlist.hash(key, bucketlist.maxbuckets)
-  let newentry =  {key: key, value : value, 'next': null}
+  console.log(hashed)
 
-  if (bucketlist.nodes[hashed].head === null) {
-    newentry.next = bucketlist.nodes[hashed].next
-    bucketlist.nodes[hashed].next = newentry   
-  } else {
-    bucketlist.nodes[hashed].head = null
-    bucketlist.nodes[hashed].next = newentry
+
+  let newentry =  {key, value, next:null}
+
+  if (!bucketlist.buckets[hashed]) {
+  
+
+    bucketlist.buckets[hashed] = {}
+    bucketlist.buckets[hashed].head = null
+    bucketlist.buckets[hashed].next = null
+
   }
 
-  console.log(bucketlist.nodes);
+  if (bucketlist.buckets[hashed].head === null) {
+    newentry.next = bucketlist.buckets[hashed].next
+    bucketlist.buckets[hashed].next = newentry   
+  } else {
+    bucketlist.buckets[hashed].head = null
+    bucketlist.buckets[hashed].next = newentry
+  }
 
 
 }
 
-//  takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
- get(key) {
+// //  takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
+//  get(key) {
 
 
 
- }
+//  }
 
 
-//  takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
- has(key){
+// //  takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+//  has(key){
 
 
     
+// }
+
+
+// //  takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return true. If the key isn’t in the hash map, it should return false.
+// remove(key){
+
+
+    
+// }
+
+
+// // returns the number of stored keys in the hash map.
+// length() {
+
+
+    
+// }
+
+
+// //  removes all entries in the hash map.
+//  clear(){
+
+
+    
+//  }
+
+
+// // returns an array containing all the keys inside the hash map.
+// keys() {
+
+    
+// }
+
+
+// //  returns an array containing all the values.
+//  values(){
+
+    
+//  }
+
+
+// // returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
+// entries(){
+
+    
+// }
+
 }
-
-
-//  takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return true. If the key isn’t in the hash map, it should return false.
-remove(key){
-
-
-    
-}
-
-
-// returns the number of stored keys in the hash map.
-length() {
-
-
-    
-}
-
-
-//  removes all entries in the hash map.
- clear(){
-
-
-    
- }
-
-
-// returns an array containing all the keys inside the hash map.
-keys() {
-
-    
-}
-
-
-//  returns an array containing all the values.
- values(){
-
-    
- }
-
-
-// returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
-entries(){
-
-    
-}
-
-}
-
 
 
 
@@ -194,4 +186,12 @@ test.set(test,'hat', 'black')
 test.set(test,'ice cream', 'white')
 test.set(test,'jacket', 'blue')
 test.set(test,'kite', 'pink')
-test.set(test,'lion', 'golden')
+test.set(test,'test', 'test2')
+test.set(test,'test3', 'test4')
+test.set(test,'kitaacae', 'piacdcnk')
+test.set(test,'abcd', 'piacdcnk')
+test.set(test,'zxy', 'piacdcnk')
+
+
+console.log(test)
+
